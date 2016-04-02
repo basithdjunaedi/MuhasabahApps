82fa317bbd1b2d9df5611f72223a2ed94a36de8b
root <basithdjunaedi@gmail.com>
2016-04-03 03:29:00 +0700

    firstUpload

---
+++ app/src/main/java/com/tsalatsa/com/muhasabahapps/Activitas.java
@@ -0,0 +1,30 @@
+package com.tsalatsa.com.muhasabahapps;
+
+/**
+ * Created by root on 02/04/16.
+ */
+public class Activitas {
+    private int id;
+    private String name;
+
+    public Activitas(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
